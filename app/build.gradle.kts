65683399e5bed38a0c7372e50e159b7cca53dd18
Abdul Rauf <a.rauf.bscs.kpr@gmail.com>
2024-03-06 00:30:22 +0500

    bottom navigation bar implemented and arranged four fragments for eact item

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.agrilinkup"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.agrilinkup"
@@ -42,6 +42,7 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/main/java/com/example/agrilinkup/MainActivity.kt
+++ app/src/main/java/com/example/agrilinkup/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.agrilinkup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +13,112 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        var bottomNavigationBar=findViewById<BottomNavigationView>(R.id.bottomNavigationBar)
+
+        bottomNavigationBar.setOnItemReselectedListener {
+            when(it.itemId){
+                R.id.HomeFragment -> homeFragment()
+                R.id.chatsFragment -> chatsFragment()
+                R.id.CartFragment -> cartFragment()
+                R.id.profileFragment -> profileFragment()
+            }
+        }
+
+
+
+    }
+    fun homeFragment(){
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            val fragments = fragmentManager?.fragments
+            if (fragments != null) {
+                for (fragment in fragments) {
+                    if (fragment.tag == "HomeFragment"){
+
+                        //we don't want to create a new Fragment each time the user presses the button
+                        // return
+                    }
+                }
+            }
+
+            val fragment: Fragment = HomeFragment()
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+            fragmentTransaction.add(R.id.host, fragment, "HomeFragment")
+
+            fragmentTransaction.addToBackStack("HomeFragment")
+
+            fragmentTransaction.commit()
+    }
+    fun chatsFragment(){
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        val fragments = fragmentManager?.fragments
+        if (fragments != null) {
+            for (fragment in fragments) {
+                if (fragment.tag == "ChatFragment"){
+
+                    //we don't want to create a new Fragment each time the user presses the button
+                    // return
+                }
+            }
+        }
+
+        val fragment: Fragment = ChatFragment()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.host, fragment, "ChatFragment")
+
+        fragmentTransaction.addToBackStack("ChatFragment")
+
+        fragmentTransaction.commit()
+    }
+    fun cartFragment(){
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        val fragments = fragmentManager?.fragments
+        if (fragments != null) {
+            for (fragment in fragments) {
+                if (fragment.tag == "CartFragment"){
+
+                    //we don't want to create a new Fragment each time the user presses the button
+                    // return
+                }
+            }
+        }
+
+        val fragment: Fragment = CartFragment()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.host, fragment, "CartFragment")
+
+        fragmentTransaction.addToBackStack("CartFragment")
+
+        fragmentTransaction.commit()
+
+    }
+    fun profileFragment(){
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        val fragments = fragmentManager?.fragments
+        if (fragments != null) {
+            for (fragment in fragments) {
+                if (fragment.tag == "ProfileFragment"){
+
+                    //we don't want to create a new Fragment each time the user presses the button
+                    // return
+                }
+            }
+        }
+
+        val fragment: Fragment = ProfileFragment()
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.add(R.id.host, fragment, "ProfileFragment")
+
+        fragmentTransaction.addToBackStack("ProfileFragment")
+
+        fragmentTransaction.commit()
 
     }
 }
