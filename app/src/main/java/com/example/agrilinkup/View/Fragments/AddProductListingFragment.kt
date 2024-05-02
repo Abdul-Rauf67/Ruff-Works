06ab5b90a94f353c1f2e5480312255f1931bc637
Abdul Rauf <a.rauf.bscs@gamil.com>
2024-05-02 15:59:13 +0500

    Add Product Fragment designed

--- app/src/main/java/com/example/agrilinkup/View/Fragments/AddProductListingFragment.kt
+++ app/src/main/java/com/example/agrilinkup/View/Fragments/AddProductListingFragment.kt
@@ -1,4 +1,4 @@
-package com.example.agrilinkup
+package com.example.agrilinkup.View.Fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,20 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.agrilinkup.View.Fragments.AddProductListingFragment
-import com.example.agrilinkup.databinding.FragmentHomeBinding
+import com.example.agrilinkup.HomeFragment
+import com.example.agrilinkup.R
+import com.example.agrilinkup.databinding.FragmentAddProductListingBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class HomeFragment : Fragment() {
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AddProductListingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddProductListingFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
-    private lateinit var binding: FragmentHomeBinding
+    lateinit var binding: FragmentAddProductListingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,31 +40,31 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        //val view=inflater.inflate(R.layout.fragment_home, container, false)
+       // return inflater.inflate(R.layout.fragment_add_product_listing, container, false)
 
-        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        binding= FragmentAddProductListingBinding.inflate(inflater,container,false)
         return binding.root
 
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.addProductsListings.setOnClickListener {
+        binding.btnSell.setOnClickListener {
 
-            binding.viewlayout.visibility=View.INVISIBLE
-            val fragmentTansaction=childFragmentManager.beginTransaction()
-            val addProductListingFragment=AddProductListingFragment()
-            fragmentTansaction.replace(R.id.fragment_container,addProductListingFragment)
+            binding.fragmentAddProductListing.visibility = View.INVISIBLE
+            val fragmentTansaction = childFragmentManager.beginTransaction()
+            fragmentTansaction.replace(R.id.fragmentAddProdusts_Container, HomeFragment())
             fragmentTansaction.addToBackStack(null)
             fragmentTansaction.commit()
 
 
-            //findNavController().navigate(R.id.action_homeFragment_to_addProductListingFragment)
 
+        //findNavController().navigate(R.id.action_addProductListingFragment_to_homeFragment)
         }
+
     }
+
 
     companion object {
         /**
@@ -67,12 +73,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment AddProductListingFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            AddProductListingFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
