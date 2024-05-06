f5e67c2859ca5f6df46fa1583b097019e32019d8
Abdul Rauf <a.rauf.bscs@gamil.com>
2024-05-06 18:34:49 +0500

    UI updated

--- app/src/main/java/com/example/agrilinkup/View/Activities/SignUpActivity.kt
+++ app/src/main/java/com/example/agrilinkup/View/Activities/SignUpActivity.kt
@@ -1,7 +1,12 @@
 package com.example.agrilinkup.View.Activities
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +28,25 @@ class SignUpActivity : AppCompatActivity() {
         binding.registerBtn.setOnClickListener {
 
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        val context=this
+        var Seasons= arrayOf("Farmer","Consumer","Trader")
+        if (binding.accountType!=null) {
+            var adapter1= ArrayAdapter(this,android.R.layout.simple_spinner_item,Seasons)
+            binding.accountType.adapter = adapter1
+            1.also { binding.accountType.id = it }
+            binding.accountType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long){
+                    Toast.makeText(context, "Profile clicked", Toast.LENGTH_SHORT).show()
+
+
+                    Toast.makeText(context,"Spinner Position: ${position} and Season :${Seasons[position]}",Toast.LENGTH_LONG).show()
+                }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("Not yet implemented")
+                }
+            }
         }
 
 
